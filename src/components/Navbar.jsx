0dbabbc9b3fa725b465b3da72ba12b6d9adc5e58
hardikgupta2003 <hardikgupta5172@gmail.com>
2023-08-27 11:13:28 +0530

    updated bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { FaShoppingCart } from 'react-icons/fa'
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import logo from '../logo.png'
+
 
 const Navbar = () => {
   const {cart}=useSelector((state)=>state);
@@ -8,7 +10,7 @@ const Navbar = () => {
     <div >
       <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
         <NavLink to="/">  <div className="ml-5">
-          <img src='../logo.png' className="h-14"/></div></NavLink>
+          <img src={logo} className="h-14"/></div></NavLink>
       
 
         <div className='flex items-center font-medium text-slate-100 mr-5 space-x-6'>
